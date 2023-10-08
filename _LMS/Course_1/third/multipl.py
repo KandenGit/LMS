6dafd30745ad8556d782c80d53e68372e2fed9d4
KandenGit <147169047+KandenGit@users.noreply.github.com>
2023-10-08 13:30:09 +0300

    Add files via upload

---
+++ _LMS/Course_1/third/multipl.py
@@ -0,0 +1,5 @@
+num = int(input('Введите число: '))
+
+for i in range(0, 10):
+    res = i * num
+    print(f'{i} x {num} = {res}')
---
+++ _LMS/Course_1/third/sum.py
@@ -0,0 +1,6 @@
+num = int(input('Введите число: '))
+res = 0
+
+for i in range(1, num+1):
+    res += i
+    print(res)

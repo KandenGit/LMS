3a9f82f6c55dfffea21297e9f097c8b089fcdf63
KandenGit <147169047+KandenGit@users.noreply.github.com>
2023-12-29 18:59:48 +0300

    Update main.py

--- _LMS/Course_2/Third/main.py
+++ _LMS/Course_2/Third/main.py
@@ -1,12 +1,6 @@
 import pandas as pd
 
-"""
-как я понял, нужно найти 2 разных группы людей, в случае если в задании требовалось
-применять логический оператор И, то вывод будет пустым
-"""
-# возникла проблема с таблицей (заголовки слились в один заголовок) csv формата,
-# поэтому я конвертировал таблицу в Excel формат
-read_data = pd.read_excel('/content/Customers.xlsx')
+read_data = pd.read_csv('/content/Customers.csv', sep=";")
 df = pd.DataFrame(read_data)
 query_df = df.loc[(df['Age'] > 30) & (df['Income'] < 30000) | (df['Profession'] == 'Lawyer') & (df['Work Experience'] > 5)]
 
